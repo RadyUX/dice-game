f99938c6191bc02a78aa0259ebe7a0b5120c7ac7
Rafaele <rafaele.sinaguglia@gmail.com>
2023-06-04 20:35:17 +0200

    logique javascript implémtené et interace adaptative
    logique métier intégral du jeu avec message d'erreur , class active ect... les features principal sont mise en place, code a optimiser et a ameliorer

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let currentPlayer = 1
+let roundScore = [0, 0]
+let globalScore = [0, 0]
+let hasRolled = [false, false];
+
+function clearDice() {
+    const dice = document.querySelector('.dice');
+    dice.innerHTML = '';
+  }
+
+  
 function createDice(number){
     const dotPositionMatrix = {
 		1: [
@@ -36,7 +47,7 @@ function createDice(number){
 	};
 
      const dice = document.querySelector('.dice')
-
+     clearDice()
       for (const i of dotPositionMatrix[number]) {
         const dot = document.createElement('div')
 
@@ -50,9 +61,65 @@ return dice
 }
 
 const btnRollDice = document.querySelector(".roll-btn");
-
+const btnHold = document.querySelector(".hold-btn");
+const btnNewGame = document.getElementById("reset")
+ 
+btnNewGame.addEventListener("click", () =>{  
+    currentPlayer = 1;
+    roundScore = [0, 0];
+    totalScore = [0, 0];
+    hasRolled = [false, false];
+    document.querySelector(`.player1 .current-score p`).textContent = roundScore[0];
+    document.querySelector(`.player2 .current-score p`).textContent = roundScore[1];
+    document.querySelector(`.player1 .global-score`).textContent = totalScore[0];
+    document.querySelector(`.player2 .global-score`).textContent = totalScore[1];
+    
+    
+    
+  
+    alert("score rénitialisé, nouvelle partie commencé !");
+})
 
 btnRollDice.addEventListener("click", () => {
-createDice(6)
-	console.log("dé lancé")
+    if (hasRolled[currentPlayer - 1]) {
+        alert(`Le joueur ${currentPlayer} a déjà lancé les dés ce tour.`);
+        return;
+      }
+    const random = Math.floor((Math.random() * 6) + 1);
+	const dice = createDice(random);
+    document.body.appendChild(dice);
+
+    roundScore[currentPlayer - 1] += random 
+    hasRolled[currentPlayer - 1] = true;
+    document.querySelector(`.player${currentPlayer} .current-score p`).textContent = roundScore[currentPlayer - 1];
+
+  console.log(`Tour du joueur ${currentPlayer}`);
+	
 });
+
+btnHold.addEventListener("click", () => {
+    if (!hasRolled[currentPlayer - 1]) {
+        alert(`Le joueur ${currentPlayer} n'a pas encore lancé les dés.`);
+        return;
+      }
+
+    globalScore[currentPlayer - 1] += roundScore[currentPlayer - 1]; 
+  
+    document.querySelector(`.player${currentPlayer} .global-score`).textContent =  globalScore[currentPlayer - 1];
+  
+    if (currentPlayer === 2) {
+        roundScore = [0, 0];
+        document.querySelector(`.player1 .current-score p`).textContent = roundScore[0];
+        document.querySelector(`.player2 .current-score p`).textContent = roundScore[1];
+      }
+    
+        // Ajouter la classe 'active' au joueur actif et la retirer de l'autre joueur
+  document.querySelector(`.player${currentPlayer}`).classList.remove("active");
+  document.querySelector(`.player${(currentPlayer % 2) + 1}`).classList.add("active");
+
+
+    // changer le current player
+    currentPlayer = (currentPlayer % 2) + 1;
+    hasRolled = [false, false];
+    console.log(`Tour du joueur ${currentPlayer}`);
+  });
