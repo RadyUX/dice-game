b3c3821c570192324e4b4856265980ac14a8350c
Rafaele <rafaele.sinaguglia@gmail.com>
2023-06-05 12:21:22 +0200

    code optimisé et rajout de commentaire

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ let currentPlayer = 1
 let roundScore = [0, 0]
 let globalScore = [0, 0]
 
+const btnRollDice = document.querySelector(".roll-btn");
+const btnHold = document.querySelector(".hold-btn");
+const btnNewGame = document.getElementById("reset")
 
-function clearDice() {
-    const dice = document.querySelector('.dice');
-    dice.innerHTML = '';
-  }
-
-  
 function createDice(number){
     const dotPositionMatrix = {
 		1: [
@@ -54,31 +51,32 @@ function createDice(number){
         dot.classList.add('dice-dot')
         dot.style.setProperty("--top", i[0] + "%");
         dot.style.setProperty("--left", i[1] + "%");
-		dice.appendChild(dot);
+		    dice.appendChild(dot);
       }
  
 return dice
 }
 
-const btnRollDice = document.querySelector(".roll-btn");
-const btnHold = document.querySelector(".hold-btn");
-const btnNewGame = document.getElementById("reset")
- 
+
+function clearDice() {
+  const dice = document.querySelector('.dice');
+  dice.innerHTML = '';
+}
+
+
 
 function clearGame(){
   currentPlayer = 1;
     roundScore = [0, 0];
     globalScore = [0, 0];
-   
     document.querySelector(`.player1 .current-score p`).textContent = roundScore[0];
     document.querySelector(`.player2 .current-score p`).textContent = roundScore[1];
     document.querySelector(`.player1 .global-score`).textContent = globalScore[0];
-    document.querySelector(`.player2 .global-score`).textContent = globalScore[1];
-    
-    
+    document.querySelector(`.player2 .global-score`).textContent = globalScore[1];    
 } 
 
 function RollDice(){
+  // dice animation: promise resolved when over and return the last result du lancé de dé
   return new Promise(resolve => {
     let counter = 0;
     let result;
@@ -92,8 +90,6 @@ function RollDice(){
         clearInterval(interval);
         resolve(result);  
       }
-      console.log(random)
-      console.log(`result ${result}`)
     }, 30);
   });
 }
@@ -108,6 +104,7 @@ btnRollDice.addEventListener("click", async () => {
    let random = await RollDice()
   
     if (random === 1) {
+      // remise a 0 si dé tombe aléatoirement sur le 1
         alert(`tombé sur 1, retour a 0 👽 au tour du joueur ${(currentPlayer % 2) + 1}`)
         roundScore[currentPlayer - 1] = 0;
         document.querySelector(`.player${currentPlayer} .current-score p`).textContent = 0;
@@ -115,10 +112,11 @@ btnRollDice.addEventListener("click", async () => {
     document.querySelector(`.player${(currentPlayer % 2) + 1}`).classList.add("active");
 
 
-  
+    // switch player division
     currentPlayer = (currentPlayer % 2) + 1;
    
     } else {
+      // sinon rajoute le score au joueur courant et display it
         roundScore[currentPlayer - 1] += random; 
         document.querySelector(`.player${currentPlayer} .current-score p`).textContent = roundScore[currentPlayer - 1];
     }
@@ -129,32 +127,26 @@ btnRollDice.addEventListener("click", async () => {
 
 btnHold.addEventListener("click", () => {
 
+// add curent player's temporary score to the global score and display it 
   globalScore[currentPlayer - 1] += roundScore[currentPlayer - 1]; 
   document.querySelector(`.player${currentPlayer} .global-score`).textContent =  globalScore[currentPlayer - 1];
-    
+    // victory condition
   if ( globalScore[currentPlayer - 1] >=  100){
     alert(`victoire du joueur ${currentPlayer}`)
     clearGame()
   }
    
-  
+  // after player 2 has played the round is over
     if (currentPlayer === 2) {
         roundScore = [0, 0];
         document.querySelector(`.player1 .current-score p`).textContent = roundScore[0];
         document.querySelector(`.player2 .current-score p`).textContent = roundScore[1];
       }
 
-      if (roundScore[currentPlayer - 1] === 1){
-        document.querySelector(`.player${currentPlayer} .current-score p`).textContent = 0
-      }
-    
-       
+  // player switch after submitting the round score
+  currentPlayer = (currentPlayer % 2) + 1;
   document.querySelector(`.player${currentPlayer}`).classList.remove("active");
   document.querySelector(`.player${(currentPlayer % 2) + 1}`).classList.add("active");
 
 
-  
-    currentPlayer = (currentPlayer % 2) + 1;
-    
-    console.log(`tour du joueur ${currentPlayer}`);
   });
